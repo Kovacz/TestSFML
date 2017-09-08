1a8b396ac0e636a16d8bce0ac39feb476de6df07
Andrew <kovacz@ukr.net>
2017-09-08 21:04:16 +0300

    upd.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,44 @@
+//#include "level.h"
+//#include "view.h"
+//
+//int main()
+//{
+//	Level level;
+//	level.LoadFromFile("map.tmx");
+//
+//	sf::RenderWindow window(sf::VideoMode(800, 600), "Test");
+//	view.reset(sf::FloatRect(0, 0, 800, 600));
+//
+//	sf::Clock clock;
+//	while (window.isOpen())
+//	{
+//		float time = clock.getElapsedTime().asMicroseconds();
+//		clock.restart();
+//		time = time / 800;
+//
+//		sf::Event event;
+//		while (window.pollEvent(event))
+//		{
+//			if (event.type == sf::Event::Closed)
+//				window.close();
+//		}
+//
+//		viewmap(time);//функция скроллинга карты, передаем ей время sfml
+//		window.setView(view);
+//		window.clear(sf::Color(77, 83, 140));
+//		level.Draw(window);//"оживляем" камеру в окне sfml
+//		window.display();
+//
+//	}
+//
+//	return 0;
+//}
+
 #include <iostream>
+#include <windows.h>
 #include <SFML\Graphics.hpp>
+#define MTR_2PI_F 6.283185307179586f
+#define MTR_RADIAN_F 57.295779513082320876798154814105f
 
 using namespace sf; 
 using namespace std;
@@ -8,6 +47,17 @@ enum look
 {
 	right, left, down, up, downright, upright, upleft, downleft
 };
+
+float mtrAngle_f(float x, float y)
+{
+	float dir;
+
+	dir = acosf(x / sqrtf(powf(-x, 2.0f) + powf(-y, 2.0f)));
+	if (y > 0.0f)
+		dir = MTR_2PI_F - dir;
+
+	return dir * MTR_RADIAN_F;
+}
 
 bool is_equal(double x, double y) 
 {
@@ -24,42 +74,45 @@ int sign(Value Val)
 
 look lookAtMouse(int x, int y)
 {
-<<<<<<< HEAD
-	if (x == 1 && y == 0)
+	float direction = mtrAngle_f(x, y);
+	if (direction > 337.5f && direction <= 0.0f)
 	{
 		return look::right;
 	}
-	else if (x == -1 && y == 0)
-	{
-		return look::left;
-	}
-	else if (x == 0 && y == 1)
+	else if (direction > 270.5f && direction <= 290.0f)
 	{
 		return look::down;
 	}
-	else if (x == 0 && y == -1)
+	else if (direction > 170.5f && direction <= 220.0f)
+	{
+		return look::left;
+	}
+	else if (direction > 67.5f && direction <= 90.0f)
 	{
 		return look::up;
 	}
-	else if (x == 1 && y == 1)
-=======
-	if (x == 1 && y == 1)
->>>>>>> 47e277a3312cff6cc06500a51ad2d4cd7d1e4106
+	else if (direction > 300.5f && direction <= 325.0f)
 	{
 		return look::downright;
 	}
-	else if (x == 1 && y == -1)
+	else if (direction > 22.5f && direction <= 67.5f)
 	{
 		return look::upright;
 	}
-	else if (x == -1 && y == -1)
+	else if (direction > 90.5f && direction <= 220.5f)
 	{
 		return look::upleft;
 	}
-	else if (x == -1 && y == 1)
+	else if (direction > 220.5f && direction <= 290.5f)
 	{
 		return look::downleft;
 	}
+}
+
+void MoveTo(Sprite hero_sprite)
+{
+	//Vector2i coords(Mouse::getPosition(window));
+	//hero_sprite.setPosition( coords );
 }
 
 int main()
@@ -79,10 +132,9 @@ int main()
 
 	double CurrentFrame = 0;
 	Clock clock;
-
 	while ( window.isOpen() )
 	{
-		double time = clock.getElapsedTime().asMicroseconds(); 
+		float time = clock.getElapsedTime().asMicroseconds(); 
 		clock.restart();   
 		time = time / 800; 
 
@@ -92,7 +144,6 @@ int main()
 			if ( event.type == Event::Closed )
 				window.close();
 		}
-
 		if ( Mouse::isButtonPressed(Mouse::Right) )
 		{
 			
@@ -100,62 +151,76 @@ int main()
 			totalMovement.x = Mouse::getPosition(window).x - hero_sprite.getPosition().x;
 			totalMovement.y = Mouse::getPosition(window).y - hero_sprite.getPosition().y;
 
+			if (totalMovement.x > 100)
+			{
+				totalMovement.x = 100;
+			}
+			if (totalMovement.y > 100)
+			{
+				totalMovement.y = 100;
+			}
+			if (totalMovement.x < -100)
+			{
+				totalMovement.x = -100;
+			}
+			if (totalMovement.y < -100)
+			{
+				totalMovement.y = -100;
+			}
+
+
 			double dir_x = sign(totalMovement.x);
 			double dir_y = sign(totalMovement.y);
 
-			cout << dir_x << " " << dir_y << endl;
+			cout << mtrAngle_f(totalMovement.x, totalMovement.y) << endl;
 
-<<<<<<< HEAD
-			if (lookAtMouse(dir_x, dir_y) == look::up)
+			if (lookAtMouse(totalMovement.x, totalMovement.y) == look::up)
 			{
-				CurrentFrame += 0.005*time;
+				CurrentFrame += 0.005 * time;
 				if (CurrentFrame > 8)
 					CurrentFrame -= 8;
 				hero_sprite.setTextureRect(IntRect(128 * int(CurrentFrame), 254, 128, 128));
-				hero_sprite.move(totalMovement * (1.f / 1000.f));
+				hero_sprite.move(totalMovement * time * 0.001f);
 			}
-			else if (lookAtMouse(dir_x, dir_y) == look::down)
+			else if (lookAtMouse(totalMovement.x, totalMovement.y) == look::down)
 			{
 				CurrentFrame += 0.005*time;
 				if (CurrentFrame > 8)
 					CurrentFrame -= 8;
 				hero_sprite.setTextureRect(IntRect(128 * int(CurrentFrame), 762, 128, 128));
-				hero_sprite.move(totalMovement * (1.f / 1000.f));
+				hero_sprite.move(totalMovement * time * 0.001f);
 			}
-			else if (lookAtMouse(dir_x, dir_y) == look::left)
+			else if (lookAtMouse(totalMovement.x, totalMovement.y) == look::left)
 			{
 				CurrentFrame += 0.005*time;
 				if (CurrentFrame > 8)
 					CurrentFrame -= 8;
 				hero_sprite.setTextureRect(IntRect(128 * int(CurrentFrame), 0, 128, 128));
-				hero_sprite.move(totalMovement * (1.f / 1000.f));
+				hero_sprite.move(totalMovement * time * 0.001f);
 			}
-			else if (lookAtMouse(dir_x, dir_y) == look::right)
+			else if (lookAtMouse(totalMovement.x, totalMovement.y) == look::right)
 			{
 				CurrentFrame += 0.005*time;
 				if (CurrentFrame > 8)
 					CurrentFrame -= 8;
 				hero_sprite.setTextureRect(IntRect(128 * int(CurrentFrame), 508, 128, 128));
-				hero_sprite.move(totalMovement * (1.f / 1000.f));
+				hero_sprite.move(totalMovement * time * 0.001f);
 			}
 			else if (lookAtMouse(dir_x, dir_y) == look::downright)
-=======
-			if (lookAtMouse(dir_x, dir_y) == look::downright)
->>>>>>> 47e277a3312cff6cc06500a51ad2d4cd7d1e4106
 			{
 				CurrentFrame += 0.005*time; 
 				if (CurrentFrame > 8) 
 					CurrentFrame -= 8;
 				hero_sprite.setTextureRect(IntRect(128 * int(CurrentFrame), 635, 128, 128)); 
-				hero_sprite.move(totalMovement * (1.f / 1000.f));
+				hero_sprite.move(totalMovement * time * 0.001f);
 			}
 			else if (lookAtMouse(dir_x, dir_y) == look::upright)
 			{
-				CurrentFrame += 0.005*time;
+				CurrentFrame += 0.005 * time;
 				if (CurrentFrame > 8)
 					CurrentFrame -= 8;
 				hero_sprite.setTextureRect(IntRect(128 * int(CurrentFrame), 381, 128, 128));
-				hero_sprite.move(totalMovement * (1.f / 1000.f));
+				hero_sprite.move(totalMovement * time * 0.001f);
 			}
 			else if (lookAtMouse(dir_x, dir_y) == look::upleft)
 			{
@@ -163,7 +228,7 @@ int main()
 				if (CurrentFrame > 8)
 					CurrentFrame -= 8;
 				hero_sprite.setTextureRect(IntRect(128 * int(CurrentFrame), 127, 128, 128));
-				hero_sprite.move(totalMovement * (1.f / 1000.f));
+				hero_sprite.move(totalMovement * time * 0.001f);
 			}
 			else if (lookAtMouse(dir_x, dir_y) == look::downleft)
 			{
@@ -171,7 +236,7 @@ int main()
 				if (CurrentFrame > 8)
 					CurrentFrame -= 8;
 				hero_sprite.setTextureRect(IntRect(128 * int(CurrentFrame), 889, 128, 128));
-				hero_sprite.move(totalMovement * (1.f / 1000.f));
+				hero_sprite.move(totalMovement * time * 0.001f);
 			}
 				
 		}
